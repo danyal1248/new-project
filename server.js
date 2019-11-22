993a9d1667e8f952efc2b1186179881affef9f4e
danyal1248 <danyal@spectrumyr.com>
2019-11-23 00:25:48 +0500

    new files has been added

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
+// Nodejs encryption with CTR
 const express = require("express");
 const app = express();
-const bodyParser = require("body-parser");
-
-app.use(bodyParser.urlencoded({extended = True}));
-
-app.get("/",function(req,res){
-res.send("hey buddy");
+const ZKLib = require('zklib');
+ 
+ZK = new ZKLib({
+  ip: '210.210.3.105',
+  port: 80,
+  inport: 3000,
+  timeout: 5000,
+});
+ 
+// connect to access control device
+ZK.connect(function(err) {
+  if (err) throw err;
+ 
+  // read the time info from th device
+  ZK.getTime(function(err, t) {
+    // disconnect from the device
+    ZK.disconnect();
+ 
+    if (err) throw err;
+ 
+    console.log("Device clock's time is " + t.toString());
+  });
 });
 
 app.listen(3000,function(){
